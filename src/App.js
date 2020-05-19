1df24aa21d0b9106e2ec5019d63de62b4e127d8d
“elizanort” <elizabethnortman@gmail.com>
2020-05-19 11:27:32 -0400

    jsx assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    render() {
+        <body className="page_container">
+          <div className="sidebar_container">
+            <div className="logo">
+              <h1>CHECKCHECK</h1>
+            </div>
+            <div className="solidline"></div>
+            <div className="solidline"></div>
+            <div className="solidline"></div>
+    
+            <div className="todo">
+              <a href="www.google.com" className="todo_buttons todo">
+                To Do
+              </a>
+              <a href="www.google.com" className="todo_buttons grocery">
+                Grocery List
+              </a>
+              <a href="www.google.com" className="todo_buttons application">
+                Application
+              </a>
+              <a href="www.google.com" className="todo_buttons newlist">
+                New List
+                <i className="fas fa-plus"></i>
+              </a>
+            </div>
+    
+            <div className="username">{userInfo}</div>
+          </div>
+    
+          <div className="main_container">
+            <div className="header">
+              <h1 className="header_h1">ToDo List</h1>
+            </div>
+    
+            <div className="todolist">
+              <input
+                type="text"
+                className="todolist_input"
+                placeholder="Add new item"
+              ></input>
+              <button type="submit" className="todolist_submitbutton">
+                Add
+              </button>
+              <div>
+                <ul>
+                  <li className="todolist_listitems">
+                    <Todos todos={this.state.todos}/>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </body>
   );
 }
 
