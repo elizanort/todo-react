7713eaa5909ffd788e7d65d27064e9f3899496b8
“elizanort” <elizabethnortman@gmail.com>
2020-05-26 12:41:50 -0400

    todo updated

--- src/App.js
+++ src/App.js
@@ -1,68 +1,13 @@
 import React from "react";
+import shortid from "shortid";
 import "./App.scss";
 import Todo from "./components/Todo";
 import profileimg from "./imgs/profile.svg";
-import Sidebar from './components/Sidebar';
+import Sidebar from "./components/Sidebar";
 
-
-
-// const userInfo = userInformation.map(function (userInformation) {
-//   return (
-//     <>
-//       <img
-//         src={userInformation.picture}
-//         className="profile_img"
-//         alt="profile"
-//       ></img>
-
-//       <a href="www.google.com" className="username_link">
-//         {userInformation.name}
-//       </a>
-//     </>
-//   );
-// });
-
-class App extends React.Component{
+class App extends React.Component {
   state = {
-    todoList: [
-      {
-        id: 1,
-        listName: "Todo List",
-        title: "Workout",
-        isChecked: false,
-      },
-      {
-        id: 2,
-        listName: "Todo list",
-        title: "Complete ",
-        isChecked: false,
-      },
-      {
-        id: 3,
-        listName: "Grocery List",
-        title: "Grapes",
-        isChecked: false,
-      },
-      {
-        id: 4,
-        listName: "Grocery List",
-        title: "Bread",
-        isChecked: false,
-      },
-      {
-        id: 5,
-        listName: "Todo list",
-        title: "Write a thank you letter to dad",
-        isChecked: false,
-      },
-    
-      {
-        id: 6,
-        listName: "Application List",
-        title: "UXR Google",
-        completed: false,
-      },
-    ],
+    todoList: [],
 
     newTodoItem: "",
 
@@ -74,33 +19,60 @@ class App extends React.Component{
     ],
   };
 
-    // onChangeCheckbox = (event) => {
-    //   this.setState({ isChecked: event.target.checked });
-    // }
 
-    handleInputChange = (event) => {
-      this.setState({newTodoItem: event.target.value});
-    }
+  handleInputChange = (event) => {
+    this.setState({ newTodoItem: event.target.value });
+  };
 
-    handleNewTodoItem = () => {
-      let newTodoObj = {
-          id: "",
-          listName: "",
-          title: this.state.newTodoItem,
-          completed: false,
-      };
-
-      this.setState({
-        todoList: [...this.state.todoList, newTodoObj],
-        newTodoItem: "",
-      });
+  handleNewTodoItem = () => {
+    let newTodoObj = {
+      id: shortid.generate(),
+      listName: "",
+      title: this.state.newTodoItem,
+      completed: false,
     };
 
+    this.setState({
+      todoList: [...this.state.todoList, newTodoObj],
+      newTodoItem: "",
+    });
+  };
 
-  render (){
-    return(
+
+  onChangeCheckbox = (id) => {
+    this.setState((state) => {
+      const newList = state.todoList.map((item) => {
+        if (item.id === id) {
+          return {...item, completed: item.completed ? false : true};
+        } else {
+          return item;
+        }
+      });
+
+      return {
+        todoList: newList,
+      };
+    });
+  };
+
+
+  
+  deleteItem = (id) => {
+    const list = [...this.state.todoList];
+
+    const updatedList = list.filter(item => item.id !== id);
+
+    this.setState({todoList: updatedList})
+
+  }
+
+  
+
+
+  render() {
+    return (
       <div className="page_container">
-      <Sidebar userInformation={this.state.userInformation}/>
+        <Sidebar userInformation={this.state.userInformation} />
 
         <div className="main_container">
           <div className="header">
@@ -110,26 +82,30 @@ class App extends React.Component{
           <div className="todolist">
             <input
               type="text"
+              placeholder="Add new item"
               value={this.state.newTodoItem}
               onChange={this.handleInputChange}
               className="todolist_input"
-              placeholder="Add new item"
             ></input>
-            <button type="submit" className="todolist_submitbutton" onClick={this.handleNewTodoItem}>
+            <button
+              type="submit"
+              className="todolist_submitbutton"
+              onClick={this.handleNewTodoItem}
+            >
               Add
             </button>
             <div>
               <ul>
                 {this.state.todoList.map((todoItem, index) => (
-                  <Todo todoItem={todoItem} key={index} />
+                  <Todo todoItem={todoItem} onChangeCheckbox={this.onChangeCheckbox} deleteItem={this.deleteItem} key={index} />
                 ))}
               </ul>
             </div>
           </div>
         </div>
       </div>
-    )
-  };
+    );
+  }
 }
 
 export default App;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from "react";
 
 function Todo(props) {
-    return(
-        <li className="todolist_listitems">
-            <input type="checkbox"></input> 
-            <p>{props.todoItem.title}</p>
+
+
+
+    return (
+        <li className="todolist_listitems" style= {props.todoItem.completed ? {textDecoration: 'line-through'} : null}>
+        <input
+            type="checkbox"
+            onChange={ () => props.onChangeCheckbox(props.todoItem.id)
+            }
+        ></input>
+        <p>{props.todoItem.title}</p>
+        <button className="delete_button" onClick={ () =>
+        props.deleteItem(props.todoItem.id)
+        }>X</button>
         </li>
-    );
+  );
 }
 
 export default Todo;
